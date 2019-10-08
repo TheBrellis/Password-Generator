05e4cc168b25a0882de059eed17b2c7c93a6be1f
TheBrellis <brianroyalellis@gmail.com>
2019-10-08 18:23:31 -0400

    added sudo code to pwGen.js

---
+++ pwGen.js
@@ -0,0 +1,34 @@
+function pwGen(pwLength) {
+
+
+   // define an empty string called userPassword. 
+
+    //Randomly select a number called length between 8 and 128. (establish function using 4 steps through the For loop. Expand afterward.) 
+    
+  //  Step through a FOR loop the number of times as determined by the random number
+    
+  //  set up an array (could also use an object?) with 4 strings (one for each character type). Each string contains all possible values for the character type. 
+    
+  //  in the four loop. Generate a random number between 0 and 3, use this to select what index of the array is being referenced for that input. 
+    
+  //  generate another random number between 0 and the length of the selected string. assign this to index of the string.
+    
+  //  add the above character to userPassword variable.
+    
+  //  Repeat until complete
+    
+ //   at the end of the loop return userPassword. 
+    
+ //   Output:
+    
+ //   if you can use bootstrap do so. (easy peasy choice) 
+    
+  //  assign the button to run the function defined above.
+    
+  //  have the resulting variable printed to the needed area.
+    
+  //  Figure out how to make a copy to clipboard button. 
+    
+
+
+}
