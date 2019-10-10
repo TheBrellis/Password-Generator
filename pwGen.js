1f8b0c62967eebe697cf3559e0e2ab4afc219121
TheBrellis <brianroyalellis@gmail.com>
2019-10-09 20:56:53 -0400

    completed: verification of users password length (now prompts user for new length until length parameter is met), verification of character type (will continue to prompt the user to select a character type until they select atleast one)

--- pwGen.js
+++ pwGen.js
@@ -1,56 +1,62 @@
 function pwGen() {
-
-  // ############ NEXT COMMIT -m "ADDED CHARACTER STRINGS AND VERIFICATION OF USER SELECTIONS"
-
+//Hello There//
 // Defining strings for the available character types:
 
-  var charSpecial = '!#$%&)(*+,-./: ;<=>?@[\]^_`{|}~'
+  var charSpecial = '!#$%&)(*+,-./: ;<=>?@]\[^_`{|}~'
   var charUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var charLower = 'abcdefghijklmnopqrstuvwxyz';
   var charNum = '0123456789';
 
-//*** PROMPTING USER *** for password lengths and character types to include: 
-  var pwLength = prompt('How long would you like your password to be?');
+
+//*** PROMPTING USER *** for password length //
+
+  var pwLength = prompt('How long would you like your password to be? (must be between 8 and 128!)');
+//***VERIFY*** that the password length is within the parameters
+while (pwLength > 128 || pwLength < 8){
+  alert('Listen Fool, password has to be between 8 and 128 characters')
+  pwLength = prompt('Now, how long would you like your password to be?');
+}
+//*** PROMPTING USER *** for character types //
   var typeSpecial = confirm('Would you like it to contain special characters?');
   var typeUpper = confirm('Would you like it to contain uppercase characters?');
   var typeLower = confirm('Would you like it to contain lowercase characters?');
   var typeNum = confirm('Would you like it to contain numeric characters?');
 
-
-  //*** VERIFYING *** what types the user selected and setting up the typeArray accordingly
-
-  switch(pwLength,typeSpecial,typeUpper,typeLower,typeNum) {
-    //user selects all char types
-    case (typeSpecial === true && typeUpper === true && typeLower === true && typeNum === true):
-      var typeArray = [charSpecial, charUpper, charLower, charNum];
-      break;
-    //user does not want numbers
-    case (typeSpecial === true && typeUpper === true && typeLower === true && typeNum === false):
-      var typeArray = [charSpecial, charUpper, charLower];
-      break;
-    //user does not want lower case or numbers
-    case (typeSpecial === true && typeUpper === true && typeLower === false && typeNum === false):
-      var typeArray = [charSpecial, charUpper];
-      break;
-    //user does not want upper, lower, or numbers
-    case (typeSpecial === true && typeUpper === false && typeLower === false && typeNum === false):
-      var typeArray = [charSpecial];
-      alert('Only one type of character? OOOkay there big fella');
-      break;
-    //user says no to all character types
-    default:
-      alert("You have to pick something!");
-      var typeSpecial = confirm('Would you like it to contain special characters?');
-      var typeUpper = confirm('Would you like it to contain uppercase characters?');
-      var typeLower = confirm('Would you like it to contain lowercase characters?');
-      var typeNum = confirm('Would you like it to contain numeric characters?');
-      break;
+//*** VERIFY *** that the user selected a character type //
+  //setting an array that includes all possible character types
+    pwArray = [typeSpecial, typeUpper, typeLower, typeNum];
+  //removing all of the character types the user does not want 
+    for (i = pwArray.length -1; i >= 0; i--) {
+      if (pwArray[i] === false) {
+        pwArray.splice(i,1); //.splice removes a specific index from an array. 
+      }
+    }
+  //repeating prompts if the user did not pick a character type
+  while (pwArray.length === 0) {
+    alert("You've got to pick something!");
+    typeSpecial = confirm('Would you like it to contain special characters?');
+    typeUpper = confirm('Would you like it to contain uppercase characters?');
+    typeLower = confirm('Would you like it to contain lowercase characters?');
+    typeNum = confirm('Would you like it to contain numeric characters?');
+    pwArray = [typeSpecial, typeUpper, typeLower, typeNum];
+    for (i = pwArray.length -1; i >= 0; i--) {
+      if (pwArray[i] === false) {
+        pwArray.splice(i,1); //.splice removes a specific index from an array. 
+      }
+    }
   }
+//****END OF USER INPUT AND VERIFICATIONS (this is where the fun begins)****//
 
-//defining an empty string called userPassword. Password will be buit in this string.
+//defining an empty string called userPassword. Password will be built in this string.
 userPassword = '';
 
-   
+for (i=0; i < pwLength; i++ ) {
+  
+
+
+
+}
+
 
     //Randomly select a number called length between 8 and 128. (establish function using 4 steps through the For loop. Expand afterward.) 
     
@@ -78,6 +84,5 @@ userPassword = '';
     
   //  Figure out how to make a copy to clipboard button. 
     
-
 
 }
